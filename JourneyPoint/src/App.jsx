fbcaebe9eccea71adc510cdd5d6f0187ec03907f
Umaima724 <umaimamubashar19@gmail.com>
2026-07-17 15:30:23 +0500

    Update App.jsx

--- JourneyPoint/src/App.jsx
+++ JourneyPoint/src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Tours from "./pages/Tours";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/tours" element={<Tours />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Footer /> 
     </>
   )
 }
