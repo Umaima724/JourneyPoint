d81f268e1b3ea04fdec21cc6777a24f3ce5e65df
Umaima724 <umaimamubashar19@gmail.com>
2026-07-21 17:06:19 +0500

    Update Login.jsx

--- JourneyPoint/src/pages/Login.jsx
+++ JourneyPoint/src/pages/Login.jsx
@@ -1,5 +1,49 @@
+import Button from "../components/Button";
+
 function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <section className="min-h-screen flex items-center justify-center bg-blue-50">
+
+      <div className="bg-white shadow-xl rounded-xl p-8 w-full max-w-md">
+
+        <h2 className="text-3xl font-bold text-center mb-6">
+          Welcome Back
+        </h2>
+
+        <form className="space-y-5">
+
+          <div>
+            <label className="block mb-2 font-medium">
+              Email
+            </label>
+
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-full border rounded-lg px-4 py-3"
+            />
+          </div>
+
+          <div>
+            <label className="block mb-2 font-medium">
+              Password
+            </label>
+
+            <input
+              type="password"
+              placeholder="Enter your password"
+              className="w-full border rounded-lg px-4 py-3"
+            />
+          </div>
+
+          <Button text="Login" />
+
+        </form>
+
+      </div>
+
+    </section>
+  );
 }
 
 export default Login;
