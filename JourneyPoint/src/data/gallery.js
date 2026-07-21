9ff6fc74322bd55280fd222e042bd438e3df5fd4
Umaima724 <umaimamubashar19@gmail.com>
2026-07-21 17:04:28 +0500

    Update gallery.js

---
+++ JourneyPoint/src/data/gallery.js
@@ -0,0 +1,34 @@
+const gallery = [
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+    title: "Maldives Beach",
+  },
+  {
+    id: 2,
+    image: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+    title: "Mountain Adventure",
+  },
+  {
+    id: 3,
+    image: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+    title: "Forest Escape",
+  },
+  {
+    id: 4,
+    image: "https://images.unsplash.com/photo-1493558103817-58b2924bce98",
+    title: "Desert Safari",
+  },
+  {
+    id: 5,
+    image: "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1",
+    title: "City Lights",
+  },
+  {
+    id: 6,
+    image: "https://images.unsplash.com/photo-1469854523086-cc02fe5d880",
+    title: "Road Trip",
+  },
+];
+
+export default gallery;
