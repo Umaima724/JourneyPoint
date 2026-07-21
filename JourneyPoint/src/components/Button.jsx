863e1c265754211a7fc9a6382e18882d934e378d
Umaima724 <umaimamubashar19@gmail.com>
2026-07-21 17:06:53 +0500

    Update Button.jsx

--- JourneyPoint/src/components/Button.jsx
+++ JourneyPoint/src/components/Button.jsx
@@ -1,6 +1,8 @@
-function Button({ text }) {
+function Button({ text, className = "" }) {
   return (
-    <button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg transition">
+    <button
+      className={`bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition ${className}`}
+    >
       {text}
     </button>
   );
