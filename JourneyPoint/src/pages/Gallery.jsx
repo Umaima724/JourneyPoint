fd7994708833d37ada5712b8d7ace06118a9f257
Umaima724 <umaimamubashar19@gmail.com>
2026-07-21 17:04:57 +0500

    Update Gallery.jsx

--- JourneyPoint/src/pages/Gallery.jsx
+++ JourneyPoint/src/pages/Gallery.jsx
@@ -1,5 +1,33 @@
+import SectionTitle from "../components/SectionTitle";
+import GalleryCard from "../components/GalleryCard";
+import gallery from "../data/gallery";
+
 function Gallery() {
-  return <h1>Gallery Page</h1>;
+  return (
+    <section className="py-20 bg-gray-100">
+
+      <div className="max-w-7xl mx-auto px-6">
+
+        <SectionTitle
+          title="Travel Gallery"
+          subtitle="Take a look at some of our favorite travel destinations."
+        />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+          {gallery.map((item) => (
+            <GalleryCard
+              key={item.id}
+              image={item.image}
+              title={item.title}
+            />
+          ))}
+
+        </div>
+
+      </div>
+    </section>
+  );
 }
 
 export default Gallery;
