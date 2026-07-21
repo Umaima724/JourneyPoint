ed3947671c4a75a2cef904e2984b287cf51e72ed
Umaima724 <umaimamubashar19@gmail.com>
2026-07-21 17:00:58 +0500

    Update tours.js

---
+++ JourneyPoint/src/data/tours.js
@@ -0,0 +1,33 @@
+const tours = [
+  {
+    id: 1,
+    title: "Hunza Valley",
+    location: "Gilgit Baltistan",
+    duration: "5 Days",
+    price: 499,
+    image:
+      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+  },
+
+  {
+    id: 2,
+    title: "Skardu",
+    location: "Gilgit Baltistan",
+    duration: "7 Days",
+    price: 699,
+    image:
+      "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+  },
+
+  {
+    id: 3,
+    title: "Fairy Meadows",
+    location: "Pakistan",
+    duration: "6 Days",
+    price: 599,
+    image:
+      "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+  },
+];
+
+export default tours;
