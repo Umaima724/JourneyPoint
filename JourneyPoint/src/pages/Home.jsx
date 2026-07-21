47d3dbf5eb18adea72f360a9908da9396a9735b1
Umaima724 <umaimamubashar19@gmail.com>
2026-07-21 17:01:19 +0500

    Update Home.jsx

--- JourneyPoint/src/pages/Home.jsx
+++ JourneyPoint/src/pages/Home.jsx
@@ -1,9 +1,41 @@
 import Hero from "../components/Hero";
+import SectionTitle from "../components/SectionTitle";
+import TourCard from "../components/TourCard";
+
+import tours from "../data/tours";
 
 function Home() {
   return (
     <>
       <Hero />
+
+      <section className="py-20 bg-gray-100">
+
+        <div className="max-w-7xl mx-auto px-6">
+
+          <SectionTitle
+            title="Popular Tours"
+            subtitle="Choose from our most popular travel destinations around the world."
+          />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+            {tours.map((tour) => (
+              <TourCard
+                key={tour.id}
+                image={tour.image}
+                title={tour.title}
+                location={tour.location}
+                duration={tour.duration}
+                price={tour.price}
+              />
+            ))}
+
+          </div>
+
+        </div>
+
+      </section>
     </>
   );
 }
